8618f7838a5e3ce97a3af37cd6cf81d14276252e
Nishant Gangwar <nishantgangwar69@gmail.com>
2021-11-22 11:11:29 +0530

    New wheather app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: null, longitude: null };
+  }
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //we called setstate
+        this.setState({
+          lat: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+        console.log(position.coords.latitude);
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  render() {
+    // console.log(this.state.lat);
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>;
+    }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} long={this.state.longitude} />;
+    }
+    return <Spinner message="Please accept location request " />;
+  }
+}
+ReactDOM.render(<App />, document.querySelector("#root"));
